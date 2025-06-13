f6b22511adcdd6f05327fc32a8983069988cb0c5
Martinezcabj12 <martinezcabj12@gmail.com>
2025-06-13 03:58:14 -0300

    feat: grid aplicado para mantener el footer abajo

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -2,17 +2,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import Home from "./views/Home";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favoritos" element={<h1>Favoritos</h1>} />
-        <Route path="/formulario" element={<h1>Formulario</h1>} />
-      </Routes>
-      <Footer />
+      <div className="header">
+        <Header />
+      </div>
+      <div className="main">
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favoritos" element={<h1>Favoritos</h1>} />
+            <Route path="/formulario" element={<h1>Formulario</h1>} />
+          </Routes>
+        </main>
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
