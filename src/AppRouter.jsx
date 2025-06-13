7b2bf3606a71568b8e1ee7ad1242cdc5e0688a6c
Martinezcabj12 <martinezcabj12@gmail.com>
2025-06-13 04:21:37 -0300

    fix: Se agregaron etiquetas header, main y footer

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/NavBar";
+import Header from "./components/Header";
 import Home from "./views/Home";
 import Footer from "./components/Footer";
 import Favoritos from "./views/Favoritos";
@@ -7,13 +7,21 @@ import Favoritos from "./views/Favoritos";
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favoritos" element={<Favoritos />} />
-        <Route path="/formulario" element={<h1>Formulario</h1>} />
-      </Routes>
-      <Footer />
+      <div className="header">
+        <Header />
+      </div>
+      <div className="main">
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favoritos" element={<Favoritos />} />
+            <Route path="/formulario" element={<h1>Formulario</h1>} />
+          </Routes>
+        </main>
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
